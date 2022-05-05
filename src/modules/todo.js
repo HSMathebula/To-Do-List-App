40978f1b37d97b8173e523e6b15919fd09c802d6
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-05-05 20:13:47 +0200

    Fix linter errors

--- src/modules/todo.js
+++ src/modules/todo.js
@@ -1,8 +1,7 @@
 export default class ToDo {
-    constructor(index, description, completed) {
-      this.index = index;
-      this.description = description;
-      this.completed = completed;
-    }
+  constructor(index, description, completed) {
+    this.index = index;
+    this.description = description;
+    this.completed = completed;
   }
-  
+}
